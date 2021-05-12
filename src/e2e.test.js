33b1d91a57aa3de6b1e85e80e2a5841affb4f43d
alexpozzi <alexpozzi@users.noreply.github.com>
2021-05-12 17:03:59 +0200

    Add e2e tests

---
+++ src/e2e.test.js
@@ -0,0 +1,60 @@
+import 'regenerator-runtime/runtime' // needed by the SSR to be able to execute transpiled generator functions like async/await
+import fetch from 'node-fetch'
+
+const trimStringForComparison = str => str.replace(/^[\s]+|[\s]+$/gm, '').replace(/\n/g, '');
+
+describe('e2e :: static', () => {
+    it(`asserts that the page is rendered`, async () => {
+        const res = await fetch(`http://127.0.0.1/render?url=http://nginx/static.html`)
+        const content = await res.text()
+
+        expect(res.status).toBe(200)
+        expect(trimStringForComparison(content)).toBe(trimStringForComparison(`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <title>A static HTML page</title>
+                </head>
+                <body>
+                    <h1>A static HTML page</h1>
+                </body>
+            </html>
+        `))
+    })
+})
+
+describe('e2e :: dynamic', () => {
+    it(`asserts that the page is rendered`, async () => {
+        const res = await fetch(`http://127.0.0.1/render?url=http://nginx/dynamic.html`)
+        const content = await res.text()
+
+        expect(res.status).toBe(200)
+        expect(trimStringForComparison(content)).toBe(trimStringForComparison(`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <title>A dynamic HTML page</title>
+                </head>
+                <body>
+                    <h1>A dynamic HTML page</h1>
+                    <script>
+                        window.onload = event => {
+                            fetch('/dynamic.json')
+                                .then(response => response.json())
+                                .then(data => {
+                                    const newParagraph = document.createElement("p");
+                                    const newContent = document.createTextNode(data.content);
+                                    newParagraph.appendChild(newContent);
+
+                                    document.body.append(newParagraph)
+                                });
+                        };
+                    </script>
+                    <p>Some dynamic content.</p>
+                </body>
+            </html>
+        `))
+    })
+})
