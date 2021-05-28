9e939aa9c1933c603b7a31172830b2afd2021a13
alexpozzi <alexpozzi@users.noreply.github.com>
2021-05-28 11:34:45 +0200

    Add access logs and lower the log level of most logs

--- src/manager/index.js
+++ src/manager/index.js
@@ -2,39 +2,55 @@ import { T, always, call, cond, juxt, pipe, tap, test, when } from 'ramda'
 import createRequestRegistry from './requestRegistry'
 import initHttpServer from './http-server'
 
+// resolveJobDuration :: Job -> Integer
+const resolveJobDuration = job => ((new Date()).getTime() - job.data.queuedAt) / 1000
+
+// resolveStatusCodeFromError :: String -> Integer
 const resolveStatusCodeFromError = cond([
   [error => test(/timeout/i, error), always(504)],
   [error => test(/timed out/i, error), always(504)],
   [T, always(500)],
 ])
 
-// onJobCompleted :: (Logger, RequestRegistry) -> Function
-const onJobCompleted = (logger, requestRegistry) => (jobId, result) => when(
+// onJobCompleted :: (Logger, Queue, RequestRegistry) -> Function
+const onJobCompleted = (logger, queue, requestRegistry) => (jobId, result) => when(
   jobId => requestRegistry.has(jobId),
   juxt([
     jobId => requestRegistry.complete(jobId, JSON.parse(result)),
-    jobId => logger.info(`Completed job "${jobId}"`),
+    async jobId => {
+      const job = await queue.getJob(jobId)
+
+      logger.info(`${jobId} ${job.data.url} 200 ${resolveJobDuration(job)}`)
+
+      job.remove()
+    },
   ]),
 )(jobId)
 
-// onJobFailed :: (Logger, RequestRegistry) -> Function
-const onJobFailed = (logger, requestRegistry) => (jobId, error) => when(
+// onJobFailed :: (Logger, Queue, RequestRegistry) -> Function
+const onJobFailed = (logger, queue, requestRegistry) => (jobId, error) => when(
   jobId => requestRegistry.has(jobId),
   juxt([
     jobId => requestRegistry.fail(
       jobId,
       resolveStatusCodeFromError(error),
     ),
-    jobId => logger.error(`Job "${jobId}" has failed. ${error}.`),
+    async jobId => {
+      const job = await queue.getJob(jobId)
+
+      logger.error(`${jobId} ${job.data.url} ${resolveStatusCodeFromError(error)} ${resolveJobDuration(job)} ${error}`)
+
+      job.remove()
+    },
   ]),
 )(jobId)
 
 // initManager :: (Configuration, Logger, Queue) -> _
 export default (configuration, logger, queue) => call(pipe(
-  () => logger.info('Initializing manager.'),
+  () => logger.debug('Initializing manager.'),
   () => createRequestRegistry(),
-  tap(requestRegistry => queue.on('global:completed', onJobCompleted(logger, requestRegistry))),
-  tap(requestRegistry => queue.on('global:failed', onJobFailed(logger, requestRegistry))),
+  tap(requestRegistry => queue.on('global:completed', onJobCompleted(logger, queue, requestRegistry))),
+  tap(requestRegistry => queue.on('global:failed', onJobFailed(logger, queue, requestRegistry))),
   requestRegistry => initHttpServer(configuration, logger, queue, requestRegistry),
-  () => logger.info('Manager initialized.'),
+  () => logger.debug('Manager initialized.'),
 ))
--- src/worker/index.js
+++ src/worker/index.js
@@ -4,15 +4,15 @@ import render from './renderers/chrome'
 
 // initWorker :: (Configuration, Logger, Queue) -> _
 export default (configuration, logger, queue) => call(pipe(
-  () => logger.info('Initializing worker.'),
+  () => logger.debug('Initializing worker.'),
   () => queue.process(1, async job => {
-    if ((new Date()).getTime() - job.queuedAt > configuration.queue.job.stale_timeout) {
+    if ((new Date()).getTime() - job.data.queuedAt > configuration.queue.job.stale_timeout) {
       throw new TimeoutError(`Job "${job.id}" with url "${job.data.url}" timed out."`)
     }
 
-    logger.info(`Processing job "${job.id}" with url "${job.data.url}".`)
+    logger.debug(`Processing job "${job.id}" with url "${job.data.url}".`)
 
     return await render(configuration, logger)(job.data.url)
   }),
-  () => logger.info('Worker initialized.'),
+  () => logger.debug('Worker initialized.'),
 ))
