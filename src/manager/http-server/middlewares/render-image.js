b654dc9f8b6a722f783f08da98031d6b91525e68
alexpozzi <alexpozzi@users.noreply.github.com>
2024-10-16 20:03:49 +0200

    feat: add image rendering

--- src/manager/http-server/middlewares/render-image.js
+++ src/manager/http-server/middlewares/render-image.js
@@ -3,14 +3,14 @@ import { DEFAULT_JOB_OPTIONS } from '../../../queue'
 
 // default :: (Configuration, Logger, Queue, RequestRegistry) -> Express.app -> Express.app
 export default (configuration, logger, queue, requestRegistry) => app =>
-  app.get('/render', (req, res, next) => call(pipe(
-    () => logger.debug(`Render request for url "${req.query.url}" started.`),
+  app.get('/render-image', (req, res, next) => call(pipe(
+    () => logger.debug(`Image render request for url "${req.query.url}" started.`),
     ifElse(
       () => compose(complement(isNil), path(['query', 'url']))(req),
       pipe(
         () => requestRegistry.add(req, res, next),
         jobId => queue.add({
-          type: 'html',
+          type: 'image',
           url: req.query.url,
           queuedAt: Date.now(),
         }, {
